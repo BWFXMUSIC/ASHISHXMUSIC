83e211bbca922f59ab16565b8e44feb3ba7de122
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2023-12-22 10:45:22 +0530

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -1,61 +1,43 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from typing import Union
 
 from pyrogram.types import InlineKeyboardButton
 
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+import config
 from AarohiX import app
 
 
-def start_pannel(_):
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
+                text="⛩️𝐀ᴅᴅ ᴍᴜsɪᴄ 𝐁σт⛩️",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="🍧ʜᴇʟᴩ🍧",
+                callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text=_["S_B_2"], callback_data="settings_helper"
+                text="❤‍🔥sᴇᴛᴛɪɴɢs❤‍🔥", callback_data="settings_helper"
             ),
         ],
-    ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                ),
-                InlineKeyboardButton(
-                    text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                ),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                    )
-                ]
-            )
-        if SUPPORT_GROUP:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                    )
-                ]
-            )
+        [
+            InlineKeyboardButton(
+                text="💌𝗦ԩ𝐚ʏя𝗼༌🪽", url=f"https://t.me/SHAYRI_CHANNEL1"),
+            InlineKeyboardButton(
+                text="🍒‌⃰‌˶‌֟፝ᴄʜᴀɴɴᴇʟ⏎‌🝛꯭🧸", url=f"https://t.me/MUSICBOT_OWNER"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="👑 ᴍᴀɪɴᴛᴀɪɴᴇʀ 👑", user_id=OWNER),
+            InlineKeyboardButton(
+                text="💒ʙωғ sᴜᴩᴩᴏʀᴛ💒", url=f"https://t.me/BWF_MUSIC1"
+            ),
+        ],
+     ]
     return buttons
 
 
@@ -63,73 +45,32 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_8"], callback_data="settings_back_helper"
-            )
-        ]
-    ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                ),
-                InlineKeyboardButton(
-                    text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                ),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                    )
-                ]
-            )
-        if SUPPORT_GROUP:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                    )
-                ]
-            )
-    buttons.append(
+                text="⛩️𝐀ᴅᴅ ᴍᴜsɪᴄ 𝐁σт⛩️",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            ),
+        ],
         [
             InlineKeyboardButton(
-                text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ]
-    )
-    if GITHUB_REPO and OWNER:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"{GITHUB_REPO}"
-                ),
-            ]
-        )
-    else:
-        if GITHUB_REPO:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_6"], url=f"{GITHUB_REPO}"
-                    ),
-                ]
-            )
-        if OWNER:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_7"], user_id=OWNER
-                    ),
-                ]
-            )
-    buttons.append(
-        [InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")]
-    )
+                text="🍧ʜᴇʟᴩ🍧", callback_data="settings_back_helper"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="💌𝗦ԩ𝐚ʏя𝗼༌🪽", url=f"https://t.me/SHAYRI_CHANNEL1"),
+            InlineKeyboardButton(
+                text="🍒‌⃰‌˶‌֟፝ᴄʜᴀɴɴᴇʟ⏎‌🝛꯭🧸", url=f"https://t.me/MUSICBOT_OWNER"
+            ),
+        ],
+        [
+            InlineKeyboardButton(text="👑 ᴍᴀɪɴᴛᴀɪɴᴇʀ 👑", user_id=OWNER),
+            InlineKeyboardButton(
+                text="💒ʙωғ sᴜᴩᴩᴏʀᴛ💒", url=f"https://t.me/BWF_MUSIC1"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                    text="[🥀✨➪ 𝐎ωиєя ⏎】", url=f"https://t.me/L2R_KING0"
+                )
+        ],
+     ]
     return buttons
